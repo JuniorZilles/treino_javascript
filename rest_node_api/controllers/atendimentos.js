ae0a1775e86a67b7911acbdd6f0cd91f3a196458
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-09-22 11:18:28 -0300

    feat: node api insert

---
+++ rest_node_api/controllers/atendimentos.js
@@ -0,0 +1,17 @@
+const Atendimento = require('../models/atendimento');
+
+
+module.exports = app => { 
+    app.get('/atendimentos', (req, res) => {
+        Atendimento.lista(res);
+    });
+
+    app.get('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+        Atendimento.buscaPorId(id, res);
+    });
+
+    app.post('/atendimentos', (req, res) => {
+        Atendimento.adiciona(req.body, res);
+    });
+};
---
+++ rest_node_api/models/atendimento.js
@@ -0,0 +1,75 @@
+const moment = require('moment');
+const conexao = require('../infraestrutura/conexao');
+
+class Atendimento {
+    adiciona(atendimento, res) {
+        const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss');
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss');
+
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao);
+        const clientEhValido = atendimento.cliente.length >= 5;
+
+        const validadcoes = [
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: "Data deve ser maior ou igual a data atual"
+            },
+            {
+                nome: 'cliente',
+                valido: clientEhValido,
+                mensagem: "Cliente deve ter pelo menos 5 caracteres"
+            }
+        ];
+
+        const erros = validadcoes.filter(campo => !campo.valido);
+
+        const existeErros = erros.length;
+
+        if (existeErros) {
+            res.status(400).json(erros);
+        } else {
+            const atendimentoDatado = { ...atendimento, dataCriacao, data };
+
+            const sql = "INSERT INTO atendimentos SET ?";
+
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if (erro) {
+                    res.status(400).json(erro);
+                } else {
+                    res.status(201).json(resultados);
+                }
+            });
+        }
+    }
+
+    lista(res){
+         const sql = "SELECT * FROM atendimentos;";
+
+         conexao.query(sql, (erro, resultados)=>{
+            if (erro) {
+                res.status(400).json(erro);
+            } else {
+                res.status(200).json(resultados);
+            }
+         });
+    }
+
+    buscaPorId(id, res){
+        const sql = "SELECT * FROM atendimentos WHERE id = ?;";
+
+        conexao.query(sql, id, (erro, resultados)=>{
+           if (erro) {
+               res.status(400).json(erro);
+           } else {
+               if(resultados.length > 0){
+                res.status(200).json(resultados[0]);
+               }else{
+                res.status(404).json("Not Found");
+               }
+           }
+        });
+   }
+}
+
+module.exports = new Atendimento();
